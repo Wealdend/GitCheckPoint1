3d6859beff3229316ce91a0c7dc9947f3bcb96cc
Simon Blom <simon.blom@brightstraining.com>
2023-02-16 14:15:34 +0100

    Add project files.

---
+++ GitCheckPoint1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitCheckPoint1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Git checkpoint task 1.1");
+            CheckTest.testMessage("Task 2 checked");
+        }
+    }
+}
