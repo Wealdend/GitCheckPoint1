f67c7b9d286c787d46b43e37b7839eb7dc40b872
Simon Blom <simon.blom@brightstraining.com>
2023-02-16 14:34:41 +0100

    Changed number of the int 1

--- GitCheckPoint1/Program.cs
+++ GitCheckPoint1/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("This is the Git checkpoint task 1.1");
             CheckTest.testMessage("\nTask 2 checked");
 
-            int num1 = 10;
+            int num1 = 15;
             int num2 = 20;
             int result = Calculate.Add(num1, num2);
             Console.WriteLine("\nThe result of adding " + num1 + " and " + num2 + " is " + result);
